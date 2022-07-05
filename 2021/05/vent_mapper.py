e4f2545acdd29a4cf4b3d990bb57e24aa04faf91
mer-sublime <contact@remiblemus.com>
2022-07-05 17:02:32 +0200

    2021/05: Part 2

--- 2021/05/vent_mapper.py
+++ 2021/05/vent_mapper.py
@@ -1,6 +1,6 @@
-"""https://adventofcode.com/2021/day/"""
+"""https://adventofcode.com/2021/day/5"""
 from enum import Enum
-from unittest import TestCase
+import unittest
 
 INPUT_FILE = 'input.txt'
 TEST_INPUT_FILE = 'test_input.txt'
@@ -9,6 +9,7 @@ TEST_INPUT_FILE = 'test_input.txt'
 class Direction(Enum):
     HORIZONTAL = 'horizontal'
     VERTICAL = 'vertical'
+    DIAGONAL = 'diagonal'
 
 
 class Point:
@@ -47,38 +48,63 @@ class Line:
         return f'{self.start} -> {self.end}'
 
 
+class LinePartTwo(Line):
+
+    def get_points(self):
+        if points := super().get_points():    # The line is horizontal or vertical, return result.
+            return points
+
+        # Else, the line is diagonal.
+        self.direction = Direction.DIAGONAL 
+
+        x_sign = 1 if self.start.x < self.end.x else -1
+        y_sign = 1 if self.start.y < self.end.y else -1
+
+        # return [(self.start.x + step * x_sign, self.start.y + step * x_sign) for step in range(abs(self.start.x - self.end.x) + 1)]
+
+        points = []
+        for step in range(abs(self.start.x - self.end.x) + 1):
+            x = self.start.x + x_sign * step
+            y = self.start.y + y_sign * step
+            points.append((x, y))
+        return points
+
+
 class VentMapper:
     def __init__(self, test=False):
         self.input = TEST_INPUT_FILE if test else INPUT_FILE
 
-    def part_one(self):
+    def map_vents(self, line_class):
         points = []
 
         with open(self.input, 'r') as f:
             for row in f:
-                line = Line(row)
+                line = line_class(row)
                 if line_points := line.get_points():
                     points += line_points
 
         counter = dict.fromkeys(set(points), 0)
         for point in points:
             counter[point] += 1
+
         return sum([1 for point, count in counter.items() if count >= 2])
 
+    def part_one(self):
+        return self.map_vents(line_class=Line)
+
     def part_two(self):
-        with open(self.input, 'r') as f:
-            for line in f:
-                pass
+        return self.map_vents(line_class=LinePartTwo)
 
 
-class TestVentMapper(TestCase):
+class TestVentMapper(unittest.TestCase):
     def test_part_one(self):
         self.assertEqual(VentMapper(test=True).part_one(), 5)
 
-    # def test_part_two(self):
-    #     self.assertEqual(VentMapper(test=True).part_two(), True)
+    def test_part_two(self):
+        self.assertEqual(VentMapper(test=True).part_two(), 12)
 
 
 if __name__ == "__main__":
     print('Part One:', VentMapper().part_one())
-#   print('Part Two:', VentMapper().part_two())
+    print('Part Two:', VentMapper().part_two())
+
