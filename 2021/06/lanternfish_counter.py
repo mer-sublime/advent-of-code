e061cd61a33a573f731825ecd65050db60291f7e
mer-sublime <contact@remiblemus.com>
2022-07-05 17:32:34 +0200

    2021/06 with constants.

--- 2021/06/lanternfish_counter.py
+++ 2021/06/lanternfish_counter.py
@@ -4,13 +4,15 @@ from unittest.mock import patch
 
 INPUT_FILE = 'input.txt'
 TEST_INPUT_FILE = 'test_input.txt'
+NEW_FISH_TIMER = 8
+RESET_FISH_TIMER = 6
 
 
 class LanternFishCounter:
 
     def __init__(self, test=False):
         self.input = TEST_INPUT_FILE if test else INPUT_FILE
-        self.fishes = [0] * 9
+        self.fishes = [0] * (NEW_FISH_TIMER + 1)
 
     def pass_time(self, days=80):
         with open(self.input, 'r') as f:
@@ -19,24 +21,23 @@ class LanternFishCounter:
 
         print(f'Initial state: {self.count_fishes()} fishes.\t {self.fishes}')
 
-        for day in range(1, days + 1):
+        for day in range(days):
             new_fishes = self.fishes[0]
-            for i in range(8):
-                self.fishes[i] = self.fishes[i+1]   # Decrement timer of all fishes.
-            self.fishes[6] += new_fishes            # Reset 0-day timers to 6.
-            self.fishes[8] = new_fishes             # Add newborn fishes.
+            for i in range(NEW_FISH_TIMER):
+                self.fishes[i] = self.fishes[i + 1]         # Decrement timer of all fishes.
+            self.fishes[RESET_FISH_TIMER] += new_fishes     # Reset 0-day timers to 6.
+            self.fishes[NEW_FISH_TIMER] = new_fishes        # Add newborn fishes.
 
         count = self.count_fishes()
         print(f'After {days} days: {count} fishes.\t {self.fishes}\n')
         return count
 
     def count_fishes(self):
-        count = sum([count for count in self.fishes])
-        return count
+        return sum(self.fishes)
 
 
 class TestLanternFishCounter(TestCase):
-    def test_count(self):
+    def test_pass_time(self):
         self.assertEqual(LanternFishCounter(test=True).pass_time(days=18), 26)
         self.assertEqual(LanternFishCounter(test=True).pass_time(days=80), 5934)
         self.assertEqual(LanternFishCounter(test=True).pass_time(days=256), 26984457539)
