563665307782b73fb190dd15f671e354e8c82713
mer-sublime <contact@remiblemus.com>
2022-07-12 21:55:33 +0200

    2021/07 with mean + improve brute force.

--- 2021/07/crab_sorting.py
+++ 2021/07/crab_sorting.py
@@ -1,56 +1,73 @@
 """https://adventofcode.com/2021/day/7"""
+import math
+import statistics
 from unittest import TestCase
-import statistics, math
 
 INPUT_FILE = 'input.txt'
 TEST_INPUT_FILE = 'test_input.txt'
 
 
+def triangular_sum(n):
+    return n * (n + 1) // 2
+
+
 class CrabSorting:
     def __init__(self, test=False):
         self.input = TEST_INPUT_FILE if test else INPUT_FILE
+        with open(self.input, 'r') as f:
+            self.crabs = list(map(int, f.readline().split(',')))
+
+    def find_fuel_cost(self, pos):
+        """n steps require n fuel units."""
+        return sum([abs(crab - pos) for crab in self.crabs])
+
+    def find_fuel_cost_triangular(self, pos):
+        """n steps require n x (n + 1) / 2 fuel units."""
+        # return sum([sum(range(abs(crab - pos) + 1)) for crab in self.crabs])
+        return sum([triangular_sum(abs(crab - pos)) for crab in self.crabs])
 
     def part_one(self):
-        with open(self.input, 'r') as f:
-            crabs = list(map(int, f.readline().split(',')))
-
-            # Find median position
-            target_position = statistics.median(crabs)
-            #
-            # crabs.sort()
-            # n_crabs = len(crabs)
-            # if n_crabs % 2:
-            #     median_position = crabs[int((n_crabs + 1) / 2)]
-            # else:
-            #     median_position = crabs[int(n_crabs / 2)]
-
-            # Return the sum of steps needed by each crab to reach target position
-            return sum([abs(crab - target_position) for crab in crabs])
+        """Find how much fuel is needed by each crab to reach target position."""
+        return self.find_fuel_cost(pos=int(statistics.median(self.crabs)))
 
     def part_two(self):
-        with open(self.input, 'r') as f:
-            crabs = list(map(int, f.readline().split(',')))
+        """Find how much fuel is needed by each crab to reach target position."""
+        mean = statistics.mean(self.crabs)
+        floor_mean, ceil_mean = math.floor(mean), math.ceil(mean)
 
-            # Find all possible values for target position.
-            possible_crab_positions = range(min(crabs), max(crabs) + 1)
+        # print(f'{mean = }')
+        # print(f'Floor: {floor_mean} -> {self.find_fuel_cost_triangular(pos=floor_mean)}')
+        # print(f'Ceil: {ceil_mean} -> {self.find_fuel_cost_triangular(pos=ceil_mean)}')
 
-            # Try them all. Brute force rocks.
-            for target_position in possible_crab_positions:
-                # Return the amount of fuel needed by each crab to reach target position
-                fuel = sum([sum(range(abs(crab - target_position) + 1)) for crab in crabs])
-                # Is it our best result yet?
-                if 'best_result' not in locals() or fuel < best_result:
-                    best_result = fuel
-            return best_result
+        return min(
+            self.find_fuel_cost_triangular(pos=floor_mean),
+            self.find_fuel_cost_triangular(pos=ceil_mean)
+        )
 
-    # def part_two(self):
-    #     with open(self.input, 'r') as f:
-    #         crabs = list(map(int, f.readline().split(',')))
-    #
-    #         square_crabs = None
-    #         avg_crab = round(math.sqrt(round(sum(square_crabs) / len(crabs))))
-    #
-    #         return sum([sum(range(abs(crab - avg_crab) + 1)) for crab in crabs])
+    def part_two_brute_force_for(self):
+        # Find all potential target positions.
+        positions = range(min(self.crabs), max(self.crabs) + 1)
+
+        # Try them all. Brute force rocks.
+        for pos in positions:
+            # Find the amount of fuel needed for all crabs to reach target position.
+            fuel = self.find_fuel_cost_triangular(pos=pos)
+
+            # Keep the lowest value.
+            best_result = min(fuel, best_result) if 'best_result' in locals() else fuel
+
+            if fuel > best_result:    # When the result gets worse, return the previous value.
+                return best_result
+
+    def part_two_brute_force_while(self):
+        pos = min(self.crabs)
+        fuel = self.find_fuel_cost_triangular(pos)
+        # Try all possible positions until the fuel value stops decreasing.
+        while fuel >= (next_fuel := self.find_fuel_cost_triangular(pos + 1)):
+            pos += 1
+            fuel = next_fuel    # Save next result for the next iteration.
+
+        return fuel
 
 
 class TestCrabSorting(TestCase):
@@ -60,7 +77,13 @@ class TestCrabSorting(TestCase):
     def test_part_two(self):
         self.assertEqual(CrabSorting(test=True).part_two(), 168)
 
+    def test_part_two_brute_force(self):
+        self.assertEqual(CrabSorting(test=True).part_two_brute_force_for(), 168)
+        self.assertEqual(CrabSorting(test=True).part_two_brute_force_while(), 168)
+
 
 if __name__ == "__main__":
     print('Part One:', CrabSorting().part_one())
     print('Part Two:', CrabSorting().part_two())
+    print('Part Two (BF for):', CrabSorting().part_two_brute_force_for())
+    print('Part Two (BF while):', CrabSorting().part_two_brute_force_while())
