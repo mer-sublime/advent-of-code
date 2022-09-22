01fce45bed8b1b396b596625c99da0740a2c9415
mer-sublime <contact@remiblemus.com>
2022-09-22 15:51:24 +0200

    Day 2021/10 - Part 2

--- 2021/10/syntax_scoring.py
+++ 2021/10/syntax_scoring.py
@@ -45,20 +45,66 @@ class SyntaxScoring:
                         break
             return syntax_error_score
 
+    @staticmethod
+    def autocomplete_line(line):
+        expected_closing_chars = deque()
+        for c in line:
+            if c in CHARACTER_PAIRS:
+                expected_closing_chars.appendleft(CHARACTER_PAIRS[c])
+            elif c != expected_closing_chars.popleft():
+                return
+        return ''.join(expected_closing_chars)
+
+    @staticmethod
+    def get_completion_score(completion_string):
+        score = 0
+        for character in completion_string:
+            score *= 5
+            match character:
+                case ')':
+                    score += 1
+                case ']':
+                    score += 2
+                case '}':
+                    score += 3
+                case '>':
+                    score += 4
+                case _:
+                    score += 0
+        return score
+
     def part_two(self):
         with open(self.input, 'r') as f:
+            scores = []
             for line in f:
-                pass
+                completion_string = self.autocomplete_line(line.rstrip())
+                if completion_string:
+                    scores.append(self.get_completion_score(completion_string=completion_string))
+            return sorted(scores)[len(scores) // 2]
 
 
 class TestSyntaxScoring(TestCase):
     def test_part_one(self):
         self.assertEqual(26397, SyntaxScoring(test=True).part_one())
 
-    # def test_part_two(self):
-    #     self.assertEqual(True, SyntaxScoring(test=True).part_two())
+    def test_autocomplete_line(self):
+        self.assertEqual('}}]])})]', SyntaxScoring.autocomplete_line(line='[({(<(())[]>[[{[]{<()<>>'))
+        self.assertEqual(')}>]})', SyntaxScoring.autocomplete_line(line='[(()[<>])]({[<{<<[]>>('))
+        self.assertEqual('}}>}>))))', SyntaxScoring.autocomplete_line(line='(((({<>}<{<{<>}{[]{[]{}'))
+        self.assertEqual(']]}}]}]}>', SyntaxScoring.autocomplete_line(line='{<[[]]>}<{[{[{[]{()[[[]'))
+        self.assertEqual('])}>', SyntaxScoring.autocomplete_line(line='<{([{{}}[<[[[<>{}]]]>[]]'))
+
+    def test_get_completion_score(self):
+        self.assertEqual(288957, SyntaxScoring.get_completion_score(completion_string='}}]])})]'))
+        self.assertEqual(5566, SyntaxScoring.get_completion_score(completion_string=')}>]})'))
+        self.assertEqual(1480781, SyntaxScoring.get_completion_score(completion_string='}}>}>))))'))
+        self.assertEqual(995444, SyntaxScoring.get_completion_score(completion_string=']]}}]}]}>'))
+        self.assertEqual(294, SyntaxScoring.get_completion_score(completion_string='])}>'))
+
+    def test_part_two(self):
+        self.assertEqual(288957, SyntaxScoring(test=True).part_two())
 
 
 if __name__ == "__main__":
     print('Part One:', SyntaxScoring().part_one())
-    # print('Part Two:', SyntaxScoring().part_two())
+    print('Part Two:', SyntaxScoring().part_two())
