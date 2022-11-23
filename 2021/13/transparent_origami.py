9e28eea15cce695f2e063bf80c131016c1a8f4d4
mer-sublime <contact@remiblemus.com>
2022-11-23 15:28:46 +0100

    Day 2021/13 - Part 2.

--- 2021/13/transparent_origami.py
+++ 2021/13/transparent_origami.py
@@ -29,11 +29,6 @@ class FoldInstruction:
         self.axis = axis
         self.value = int(value)
 
-    def _merge_lists(self, list_a, list_b):
-        return [[
-            '#' if char_a != char_b else char_a for char_a, char_b in zip(row_a, row_b)
-        ] for row_a, row_b in zip(list_a, list_b)]
-
     def fold(self, grid: list[list], debug=False) -> list[list]:
         if debug:
             print(f'Folding along {self.axis}={self.value}')
@@ -45,18 +40,25 @@ class FoldInstruction:
                 return self.fold_horizontally(grid=grid)
 
     def fold_vertically(self, grid: list[list]) -> list[list]:
-        return self.fold_horizontally(grid=list(zip(*grid)))
+        # Flip the grid
+        flipped_grid = list(zip(*grid))
+        # Fold the flipped grid
+        folded_grid = self.fold_horizontally(grid=flipped_grid)
+        # Flip the folden grid back
+        return list(zip(*folded_grid))
 
     def fold_horizontally(self, grid: list[list]) -> list[list]:
-        # @TODO: Simplify this
-        grid_top = grid[:self.value]
-        grid_bottom = grid[self.value + 1:]
-        merged_grid = list(zip(reversed(grid_top), grid_bottom))
-        merged_grid.reverse()
-        return [
-            ['#' if char_a != char_b else char_a for char_a, char_b in zip(row_a, row_b)]
-            for row_a, row_b in merged_grid]
-#        return self._merge_lists(grid_top, grid_bottom_reversed)
+        def merge_lines(line_a: list[str], line_b: list[str]) -> list[str]:
+            """Merge two lines in one."""
+            def merge_chars(a: str, b: str) -> str:
+                """Return the DOT symbol if there is at least one and the EMPTY symbol otherwise."""
+                return SYMBOLS.DOT.value if SYMBOLS.DOT.value in (a, b) else SYMBOLS.EMPTY.value
+
+            return [merge_chars(a, b) for a, b in zip(line_a, line_b)]
+
+        top_half = grid[:self.value]
+        bottom_half = grid[self.value + 1:]
+        return [merge_lines(line_a, line_b) for line_a, line_b in zip(top_half, reversed(bottom_half))]
 
 
 class PaperFolder:
@@ -66,13 +68,13 @@ class PaperFolder:
         self.grid, self.points, self.instructions = [], [], []
         with open(self.input, 'r') as f:
             # Init points
-            while point := next(f).strip():
+            while point := next(f).rstrip():
                 point = point.split(',')
                 self.points.append((int(point[0]), int(point[1])))
 
             # Parse instructions
             for line in f:
-                instruction = re.match(pattern=INSTRUCTION_PATTERN, string=line.strip())
+                instruction = re.match(pattern=INSTRUCTION_PATTERN, string=line.rstrip())
                 self.instructions.append(FoldInstruction(axis=instruction[1], value=instruction[2]))
 
         # Init grid
@@ -82,13 +84,16 @@ class PaperFolder:
         # Create empty grid
         self.max_row = max((point[0] for point in self.points))
         self.max_col = max((point[1] for point in self.points))
-        self.grid = [
+        grid = [
             [SYMBOLS.EMPTY.value for y in range(self.max_row + 1)]
             for x in range(self.max_col + 1)
         ]
         # Mark dots
         for point in self.points:
-            self.grid[point[1]][point[0]] = SYMBOLS.DOT.value
+            grid[point[1]][point[0]] = SYMBOLS.DOT.value
+
+        # Cast rows into strings
+        self.grid = grid
 
     def print_grid(self):
         for row in self.grid:
@@ -103,7 +108,6 @@ class PaperFolder:
     def part_one(self):
         # Apply only the first fold instruction
         self.grid = self.instructions[0].fold(self.grid)
-
         return self.count_dots()
 
     def part_two(self):
@@ -173,4 +177,7 @@ class TestPaperFolder(TestCase):
 
 if __name__ == "__main__":
     print('Part One:', PaperFolder().part_one())
-    print('Part Two:', PaperFolder().part_two())
+
+    folder = PaperFolder()
+    print('Part Two:', folder.part_two())
+    folder.print_grid()
