8763f0f3584af6289024e22970d746297b2c8633
mer-sublime <contact@remiblemus.com>
2022-11-23 15:39:15 +0100

    Day 2021/13 - Part 2 (kwargs).

--- 2021/13/transparent_origami_points.py
+++ 2021/13/transparent_origami_points.py
@@ -61,64 +61,48 @@ class FoldInstruction:
         return [merge_lines(line_a, line_b) for line_a, line_b in zip(top_half, reversed(bottom_half))]
 
 
-class PaperFolder:
+class PointsFolder:
     def __init__(self, test=False):
         self.input = TEST_INPUT_FILE if test else INPUT_FILE
         self.max_row, self.max_col = 0, 0
-        self.grid, self.instructions, points = [], [], []
+        self.points, self.instructions = [], []
         with open(self.input, 'r') as f:
             # Init points
             while point := next(f).rstrip():
                 point = point.split(',')
-                points.append((int(point[0]), int(point[1])))
+                self.points.append((int(point[0]), int(point[1])))
 
             # Parse instructions
             for line in f:
                 instruction = re.match(pattern=INSTRUCTION_PATTERN, string=line.rstrip())
                 self.instructions.append(FoldInstruction(axis=instruction[1], value=instruction[2]))
 
-        # Init grid
-        self.init_grid(points=points)
-
-    def init_grid(self, points: list[tuple[int, int]]):
-        # Create empty grid
-        self.max_row = max((point[0] for point in points))
-        self.max_col = max((point[1] for point in points))
-        grid = [
-            [SYMBOLS.EMPTY.value for y in range(self.max_row + 1)]
-            for x in range(self.max_col + 1)
-        ]
-        # Mark dots
-        for point in points:
-            grid[point[1]][point[0]] = SYMBOLS.DOT.value
-
-        # Cast rows into strings
-        self.grid = grid
-
     def print_grid(self):
-        for row in self.grid:
-            print(''.join(row))
+        max_x = max([point[0] for point in self.points])
+        max_y = max([point[1] for point in self.points])
+        for y in range(max_y + 1):
+            row = ''
+            for x in range(max_x + 1):
+                row += '#' if (x, y) in self.points else '.'
+            print(row)
 
     def count_dots(self) -> int:
-        if not self.grid:
-            return 0
-
-        return sum(row.count(SYMBOLS.DOT.value) for row in self.grid)
+        return len(self.points)
 
     def part_one(self):
         # Apply only the first fold instruction
-        self.grid = self.instructions[0].fold(grid=self.grid)
+        self.points = self.instructions[0].fold(points=self.points)
         return self.count_dots()
 
     def part_two(self):
         for instruction in self.instructions:
-            self.grid = instruction.fold(grid=self.grid)
+            self.grid = instruction.fold(self.grid)
         return self.count_dots()
 
 
-class TestPaperFolder(TestCase):
+class TestPointsFolder(TestCase):
     def test_part_one(self):
-        self.assertEqual(17, PaperFolder(test=True).part_one())
+        self.assertEqual(17, PointsFolder(test=True).part_one())
 
     def test_part_one_print(self):
         expected_output = '''
@@ -130,7 +114,7 @@ class TestPaperFolder(TestCase):
             ...........
             ...........
         '''
-        folder = PaperFolder(test=True)
+        folder = PointsFolder(test=True)
         folder.part_one()
         with patch('sys.stdout', new=StringIO()) as mock_stdout:
             folder.print_grid()
@@ -155,7 +139,7 @@ class TestPaperFolder(TestCase):
             #.#........
         '''
         with patch('sys.stdout', new=StringIO()) as mock_stdout:
-            PaperFolder(test=True).print_grid()
+            PointsFolder(test=True).print_grid()
             self.assertEqual(textwrap.dedent(expected_grid).strip(), mock_stdout.getvalue().strip())
 
     def test_part_two_print(self):
@@ -168,7 +152,7 @@ class TestPaperFolder(TestCase):
             .....
             .....
         '''
-        folder = PaperFolder(test=True)
+        folder = PointsFolder(test=True)
         folder.part_two()
         with patch('sys.stdout', new=StringIO()) as mock_stdout:
             folder.print_grid()
@@ -176,8 +160,8 @@ class TestPaperFolder(TestCase):
 
 
 if __name__ == "__main__":
-    print('Part One:', PaperFolder().part_one())
+    print('Part One:', PointsFolder().part_one())
 
-    folder = PaperFolder()
+    folder = PointsFolder()
     print('Part Two:', folder.part_two())
     folder.print_grid()
