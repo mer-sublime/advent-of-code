48d818f71127bcc951680d66446b56b4c68b610a
mer-sublime <contact@remiblemus.com>
2022-12-02 01:15:59 +0100

    Update 2021/Base

--- 2021/Base/base.py
+++ 2021/Base/base.py
@@ -8,24 +8,23 @@ TEST_INPUT_FILE = 'test_input.txt'
 class Puzzle:
     def __init__(self, test=False):
         self.input = TEST_INPUT_FILE if test else INPUT_FILE
+        with open(self.input, 'r') as f:
+            for line in f:
+                pass
 
     def part_one(self):
-        with open(self.input, 'r') as f:
-            for line in f:
-                pass
+        pass
 
     def part_two(self):
-        with open(self.input, 'r') as f:
-            for line in f:
-                pass
+        pass
 
 
 class TestPuzzle(TestCase):
     def test_part_one(self):
         self.assertEqual(True, Puzzle(test=True).part_one())
 
-    def test_part_two(self):
-        self.assertEqual(True, Puzzle(test=True).part_two())
+    # def test_part_two(self):
+    #     self.assertEqual(True, Puzzle(test=True).part_two())
 
 
 if __name__ == "__main__":
